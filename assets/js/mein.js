5bf2341b4d4b2d43e9a54ce7a284c4a88b8e806d
Msqoop <supercoder@supercoders-air.speedport.ip>
2023-01-04 01:33:45 +0100

    auf.1.1._1.5_1.6

---
+++ assets/js/mein.js
@@ -0,0 +1,78 @@
+
+//Aufgabe Variablen
+//================
+
+let a = 12;
+let b = a*3;
+console.log(b);
+// Ergebnis:36
+document.write("<h2>Das Ergebis ist 36</h2>");
+
+// Es hat geklappt!:)
+
+
+/* console.log(document.URL);
+ // es wird ein LInk zur der html.index Seite in der Konsole dargestellt!
+
+// Aufgabe - Konsole
+//==================
+
+let helloWorld = "Hallo World";
+console.log(helloWorld);
+
+let aaa = "Anton";
+console.log(aaa);
+
+let firstName = "Anton";
+console.log(firstName);
+
+let age = 28;
+console.log (age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = "true";
+console.log (married);
+
+console.log (aaa +" ist " + age +" Jahre alt," +" vom Beruf " + job + " und ist verheiratet" + ": "+ married + ".")
+
+// Aufgaben zur Variablen
+//=========================
+
+//1.5
+
+let car = "BMW"
+console.log(car)
+
+let x = 150
+console.log(x)
+
+let y = 50
+console.log(y)
+
+let z = x + y
+console.log(z)
+ */
+/* let firstName = "John";
+let lastName = "Doe";
+let age = 35;
+
+console.log( firstName, lastName, age)
+ */
+
+//Aufgabe 1.6 - Variablen
+
+let x = 34;
+console.log(x)
+x = 67;
+console.log(x);
+// Der Wert wurde geändert; in der Konsole als 67!
+
+const y = 34;
+console.log(y);
+/* 
+y = 67;
+console.log(Y);
+ */
+// Der Wert lässt sich nicht ändern!
